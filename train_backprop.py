a952db3b6d38534d1f54811e95160bb80d20046b
Sander <sanderjunk@posteo.de>
2024-04-25 22:00:06 +0200

    removed unnecessary print

--- train_backprop.py
+++ train_backprop.py
@@ -101,12 +101,10 @@ def test_loop(dataloader, model, loss_func):
         for X, y in dataloader:
             pred = model(X)
             test_loss += loss_func(pred, y)
-            
             correct += (pred.argmax(1) == y.argmax(1)).type(torch.float).sum().item()
-            print(f"Correct: {correct}")
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg. loss: {test_loss:>8f}\n")
+    print(f"Test Error:\nAccuracy: {(100*correct):>0.1f}%, Avg. loss: {test_loss:>8f}\n")
 
 torch.set_default_dtype(torch.float64)
 train_set = CustomTrainSet("./tt_data/train_annotations.csv", transform=normalize, target_transform=translate)
@@ -121,7 +119,7 @@ batch_size = 100
 epochs = 2
 
 loss_func = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.6)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.8)
 
 for t in range(epochs):
     print(f"Epoch {t+1}\n---------------------------------")
